ae146409f93678a19b16121e87dbb827b12f67f4
Philipp Pichler <philipp.pichler@rise-world.com>
2022-10-29 09:11:14 +0200

    add google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -8,16 +8,40 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+
+import defaultStyle from "public/style/default.css";
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
 
+
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: defaultStyle
+    }
+  ]
+}
+
 export default function App() {
   return (
     <html lang="en">
       <head>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-FPQ3K7G5QW"></script>
+		<script dangerouslySetInnerHTML={{__html:`
+		var doNotTrack = false;
+		if (!doNotTrack) {
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'G-FPQ3K7G5QW', { 'anonymize_ip': false });
+		}`}}
+		/>
         <Meta />
         <Links />
       </head>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,105 @@
+const Introduction = () => {
+	return <><p>Are you tired of spending hours on end writing and editing your content? Do you wish there was a way to
+		create high-quality, engaging content without all the hassle and time investment? Look no further, because the
+		solution is here! Introducing a range of tools that can help you create content quickly and easily.</p>
+
+		<p>With our user-friendly interfaces and powerful features, you can craft engaging and informative content that
+			captivates your audience. Whether you're a writer, blogger, or content marketer, these tools have everything
+			you
+			need to take your content to the next level.</p>
+
+		<p>Our tools cover every step of the content creation process, from brainstorming and organization to writing
+			and
+			editing. With features like keyword research and suggestion, grammar and spelling check, and readability
+			analysis,
+			you can ensure that your content is not only well-written, but also optimized for search engines and easy to
+			read.</p>
+
+		<p>But that's not all – our tools also offer a range of customizable templates and design options, so you can
+			create
+			visually appealing content that stands out from the crowd. From social media posts and infographics to blog
+			headers and email newsletters, you can create a wide range of visuals that match your brand and
+			aesthetic.</p>
+
+		<p>And best of all, our tools are designed to be intuitive and user-friendly, so you don't need any design or
+			coding
+			skills to use them. Whether you're a design pro or a complete beginner, you can easily create stunning
+			visuals in
+			minutes.</p>
+
+		<p>So why waste time and effort on tedious content creation tasks? Try our tools now and see how easy it is to
+			create fast, high-quality content that gets results. With our powerful features and user-friendly
+			interfaces, you
+			can create engaging and informative content that captivates your audience and drives traffic to your
+			website.
+			Visit our <a href="https://www.meta-mapper.com">website</a> to learn more and start creating content like a
+			pro.</p>
+	</>;
+}
+
+const MetaMapper = () => {
+	return <><p>Introducing Meta-Mapper, the all-in-one design solution for creating stunning visuals in minutes. With
+		our
+		user-friendly interface and wide range of customizable templates, you can easily create professional-looking
+		graphics for your blog, social media, or business.</p>
+
+		<p>Whether you're a design pro or a complete beginner, Meta-Mapper has everything you need to create stunning
+			visuals that stand out from the crowd. Our intuitive drag-and-drop editor lets you customize every aspect of
+			your
+			designs, from the layout and colors to the fonts and images. Plus, with our vast library of templates, you
+			can
+			quickly and easily create graphics that match your brand and aesthetic.</p>
+
+		<p>With Meta-Mapper, you can create a wide range of visuals, including social media posts, blog headers,
+			infographics, and more. We even have templates specifically designed for popular social media platforms like
+			Facebook, Instagram, and Pinterest, so you can create visuals that are perfectly sized and formatted for
+			each
+			platform.</p>
+
+		<p>So why settle for bland, generic graphics when you can create beautiful, unique visuals with Meta-Mapper? Try
+			it
+			out now and see for yourself how easy it is to create professional-looking designs in minutes. Visit <a
+				href="http://www.meta-mapper.com">Meta-Mapper.com</a> to get started.</p>
+	</>
+}
+
+const Pros = () => {
+	return <ul>
+		<li>User-friendly interface: Meta-Mapper has a user-friendly interface that is easy to navigate and use, even
+			for beginners. This allows you to quickly and easily create stunning visuals without needing any design or
+			coding skills.
+		</li>
+
+		<li>Customizable templates: Meta-Mapper offers a wide range of customizable templates, so you can create
+			graphics that match your brand and aesthetic. Whether you're creating social media posts, blog headers,
+			infographics, or something else, you can easily customize the templates to fit your needs.
+		</li>
+
+		<li>Drag-and-drop editor: Meta-Mapper's drag-and-drop editor lets you easily and intuitively customize
+			your designs. You can move and resize elements, change the colors, fonts, and images, and add text
+			and shapes to your designs with just a few clicks.
+		</li>
+
+		<li>Wide range of design options: Meta-Mapper offers a wide range of design options, so you can
+			create visuals that are unique and eye-catching. You can choose from different layout options,
+			color schemes, fonts, images, and more to create graphics that match your brand and style.
+		</li>
+
+		<li>Optimized for search engines: Meta-Mapper's features include keyword research and
+			suggestion, grammar and spelling check, and readability analysis, so you can ensure that
+			your content is not only well-written, but also optimized for search engines. This can help
+			you improve your search engine rankings and drive more traffic to your website.
+		</li>
+
+	</ul>
+}
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	return <>
+		<h1>Fast-Content.com</h1>
+		<Introduction/>
+		<h2>Meta-Mapper.com</h2>
+		<MetaMapper/>
+		<Pros />
+	</>;
 }
