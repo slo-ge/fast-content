8a5d06c9affc44d5a49cf02fe686f3d02879ffb3
Philipp Pichler <philipp.pichler.ext@porscheinformatik.com>
2023-07-21 12:31:20 +0200

    add sitemap generation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,6 +3,7 @@ import type { LoaderArgs } from '@remix-run/cloudflare';
 import { json } from '@remix-run/cloudflare';
 import tools from '../../public/tools.json';
 import type { ITool } from '~/routes/tool.$toolid';
+import React from 'react';
 
 const Introduction = () => {
     return <><p className="my-5">Are you tired of spending hours on end writing and editing your content? Do you wish
@@ -58,7 +59,7 @@ export default function Index() {
             <img className="w-10" alt="Shows logo of fast-content.com" src="/logo.png"/>
             <h1>Fast-Content.com</h1>
         </header>
-        <Introduction></Introduction>
+        <Introduction />
         <img alt="showing a computer man" src="/header-image.webp" style={{width: '100%'}}/>
         <div className="grid grid-cols-2 gap-3">
             {data.map((t, i) => <div key={i + 'outer'}>
---
+++ tools/sitemap.js
@@ -0,0 +1,36 @@
+const {SitemapStream, streamToPromise} = require('sitemap');
+const {Readable} = require('stream');
+const fs = require('fs');
+
+const BASE_URL = 'https://fast-content.com';
+
+const STATIC_URLS = [
+    {url: BASE_URL, changefreq: 'daily', priority: 1},
+    {url: `${BASE_URL}/imprint`, changefreq: 'monthly', priority: 1}
+];
+
+async function create() {
+    const data = fs.readFileSync('./public/tools.json', 'utf8');
+    const list = JSON.parse(data);
+
+
+    const links = STATIC_URLS;
+    for (const item of list) {
+        if (item.shortName) {
+            links.push({
+                url: `${BASE_URL}/tool/${item.shortName}`,
+                priority: 0.8,
+                changefreq: "monthly"
+            })
+        }
+    }
+    const stream = new SitemapStream({hostname: 'https://...'});
+    const d2 = await streamToPromise(Readable.from(links).pipe(stream));
+
+    fs.writeFile('./public/sitemap.xml', d2.toString(), () => {
+    });
+}
+
+void create();
+
+
